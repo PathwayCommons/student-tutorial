62acb7780ca51d620db24c0ea8bd93bd17a79d0d
Max Franz <maxkfranz@gmail.com>
2017-09-12 14:18:11 -0400

    React example component with a button

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('hello react');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const h = require('react-hyperscript');
+
+class Component extends React.Component {
+  constructor( props ){
+    super( props );
+
+    this.state = {
+      clicks: 0
+    };
+  }
+
+  click(){
+    let s = this.state;
+
+    this.setState({ clicks: s.clicks + 1 });
+  }
+
+  render(){
+    let clicks = this.state.clicks;
+
+    return h('button.component', {
+      id: this.props.foo,
+      onClick: event => this.click()
+    }, `Hello world ${clicks}!`);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+  ReactDOM.render(
+    h(Component, { foo: 'bar' }),
+    // <Component foo="bar"></Component>
+    document.getElementById('root')
+  );
+});
